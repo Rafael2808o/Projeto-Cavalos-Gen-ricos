f0e1dbc0ad7e6075572d0384b37b49c787c04a33
Rafael De Oliveira Silva <rafael.o.silva30@aluno.senai.br>
2025-12-08 12:37:03 -0300

    .

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ import adminRotas from './routes/admin.js';
 import categoriaRotas from './routes/categorias.js'
 import produtoRotas from './routes/produtos.js'
 import usuarioRotas from './routes/usuarios.js'
-// import pgSession from 'connect-pg-simple';
+import pgSession from 'connect-pg-simple';
 
 
 import movimentacaoRotas from './routes/movimentacao.js';
@@ -18,7 +18,7 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename);
 
 const app = express();
-// const PgSession = pgSession(session)
+const PgSession = pgSession(session)
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs')
@@ -28,29 +28,31 @@ app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 
 
-app.use(
-    session({
-        secret: "sesisenai",
-        resave: false,
-        saveUninitialized: false,
-    })
-);
+// app.use(
+//     session({
+//         secret: "sesisenai",
+//         resave: false,
+//         saveUninitialized: false,
+//     })
+// );
 
-// app.set('trust proxy', 1) // trust first proxy
-// app.use(session({
-//   store: new PgSession({
-//     conString: "postgres://postgres.hjaocdmjqkmukktcezbc:4nnBeSi1k0iJJSeB@aws-1-sa-east-1.pooler.supabase.com:6543/postgres",
-//     tableName: 'session',
-//   }),
-//   secret: "sesisenai",
-//   resave: false,
-//   saveUninitialized: false,
-//   cookie: {
-//      maxAge: 1000 * 60 * 60 * 24, // 1 day
-//      secure: true,
-//      sameSite: 'none',
-//   },
-// }));
+app.set('trust proxy', 1) // trust first proxy
+app.use(session({
+  store: new PgSession({
+    conString: "postgres://postgres.kdcjgumytrmricqjmqwm:m9N7JAQR3w4KVUnw@aws-1-sa-east-1.pooler.supabase.com:6543/postgres",
+    tableName: 'session',
+  }),
+  secret: "sesisenai",
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+     maxAge: 1000 * 60 * 60 * 24, // 1 day
+     secure: true,
+     sameSite: 'none',
+  },
+}));
+
+
 
 
 app.get('/', (req, res) => res.render('landing/index'))
@@ -68,13 +70,12 @@ const verificarAutenticacao = (req, res, next) =>{
     }
 }
 
-app.use('/admin', loginRotas) 
-app.use('/admin', adminRotas)
-app.use('/categorias', categoriaRotas)
-app.use('/produtos', produtoRotas)
-app.use('/usuarios', usuarioRotas)
-app.use('/movimentacao', movimentacaoRotas);
-
+app.use('/auth', loginRotas) 
+app.use('/admin',verificarAutenticacao, adminRotas)
+app.use('/categorias', verificarAutenticacao, categoriaRotas)
+app.use('/produtos', verificarAutenticacao, produtoRotas)
+app.use('/usuarios', verificarAutenticacao, usuarioRotas)
+app.use('/movimentacao', verificarAutenticacao, movimentacaoRotas);
 
 app.listen(3000, () =>
 console.log('Servidor rodando em http://localhost:3000')
--- db.js
+++ db.js
@@ -4,21 +4,23 @@ import pkg from 'pg';
 const { Pool } = pkg;
 
 
-const BD = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'empresa_cavalos',
-    password: 'admin',
-    port: 5432
-});
-
 // const BD = new Pool({
-//     user: 'postgres',                   // CORRETO
-//     host: 'aws-1-sa-east-1.pooler.supabase.com',
-//     database: 'postgres',
-//     password: '4nnBeSi1k0iJJSeB',      // sua senha
-//     port: 6543,
-//     ssl: { rejectUnauthorized: false }
+//     user: 'postgres',
+//     host: 'localhost',
+//     database: 'empresa_cavalos',
+//     password: 'admin',
+//     port: 5432
 // });
 
+const BD = new Pool({
+    user: 'postgres.kdcjgumytrmricqjmqwm',                   // CORRETO
+    host: 'aws-1-sa-east-1.pooler.supabase.com',
+    database: 'postgres',
+    password: 'm9N7JAQR3w4KVUnw',      // sua senha
+    port: 6543,
+    ssl: { rejectUnauthorized: false }
+});
+
+
+// postgres://postgres.kdcjgumytrmricqjmqwm:m9N7JAQR3w4KVUnw@aws-1-sa-east-1.pooler.supabase.com:6543/postgres
 export default BD;
